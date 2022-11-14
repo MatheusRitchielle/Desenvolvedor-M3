baee1aee47869d303acec2bc68bf8c1c04a23396
MatheusRitchielle <matheusritchielle98@gmail.com>
2022-11-14 17:31:05 -0300

    size filter

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
+const btnteste = document.getElementById(".ver-cores");
+
+function teste(){
+  alert("Agora foi")
+}
+
 const produtoGet = (url) => {
   let request = new XMLHttpRequest()
   request.open("GET", url, false)
@@ -7,7 +13,7 @@ const produtoGet = (url) => {
 
 const createCard = (products) => {
   const card = document.createElement("div");
-  card.classList.add("card-container");//verificar se removendo essa parte a organização dos products vai ser feita
+  card.classList.add("card-container");
 
   card.innerHTML = `
   <div class="card-info" style="display: flex; align-items:center; flex-direction: column;"
@@ -16,7 +22,6 @@ const createCard = (products) => {
     <div style="font-weight: bold;">R$ ${products.price}</div>
     <div style="color: #666666">até ${products.parcelamento[0]}x de R$ ${products.parcelamento[1]}</div>
     <button 
-      onClick="saveCarShop()";
       style="background-color: black; 
       color: #fff; 
       height: 2.3rem;
@@ -24,26 +29,12 @@ const createCard = (products) => {
       width: 100%;
       cursor: pointer"
       >
+      <script>saveCarShop()</script>
         COMPRAR
     </button>
   </div>
   `
   return card;
-}
-
-const sortCard = () => {
-  const boxSort = document.createElement("div");
-
-  boxSort.innerHTML = `
-    <select name="OrdenarPor" id="OrdenarPor">
-      <option value="" disabled selected>Ordenar por:</option>
-      <option value="maisRecentes">Mais Recentes</option>
-      <option value="menorPreco">Menor Preço</option>
-      <option value="maiorPreco">Maior Preço</option>
-    </select>
-  `;
-
-
 }
 
 const main = () => {
@@ -57,15 +48,16 @@ const main = () => {
 }
 
 const filter = (filtroOrdenacao, maiorMenor, filtroCor, filtroTamanho) => {
-  var filtroOrdenacao = "date"
-  var maiorMenor = "desc"
+  var filtroOrdenacao = "date"//||price
+  var maiorMenor = "desc" //|| asc
   var filtroCor = "Rosa"
-  var filtroTamanho = "GG"
+  var filtroTamanho = "P"
 
   if (filtroOrdenacao === "date") {
     ordenaPor = "date"
     descAsc = "desc"
   }
+  
   else if (filtroOrdenacao === "price") {
     ordenaPor = "price"
     if (maiorMenor === "asc") {
@@ -74,7 +66,6 @@ const filter = (filtroOrdenacao, maiorMenor, filtroCor, filtroTamanho) => {
       descAsc = "desc"
     }
   }
-
 
   let data = produtoGet(`http://localhost:5000/products/?_sort=${ordenaPor}&_order=${descAsc}&color=${filtroCor}&size=${filtroTamanho}`)
   let products = JSON.parse(data)
@@ -85,4 +76,19 @@ const filter = (filtroOrdenacao, maiorMenor, filtroCor, filtroTamanho) => {
   });
 }
 
-filter()
+const filterPrice = (precoMin, precoMax) => {
+  precoMin = 0;
+  precoMax = 50;
+
+  let data = produtoGet(`http://localhost:5000/products/?price>${precoMin}&price>${precoMax}`)
+  let products = JSON.parse(data)
+  const card = document.querySelector("card");
+  products.forEach(element => {
+    let dados = createCard(element);
+    card.appendChild(dados)
+  });
+}
+
+main()
+
+//
